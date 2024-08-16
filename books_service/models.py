e05f6aae38b6b1d2cc1261d403449fbe195ee8c3
Olga <olgahrytsiuk777@gmail.com>
2024-08-16 17:13:47 +0200

    added models.py for app books_service

--- books_service/models.py
+++ books_service/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD"
+        SOFT = "SOFT"
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(max_length=100, choices=CoverChoices.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
